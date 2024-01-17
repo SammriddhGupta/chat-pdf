9e9689fe196153623c44e5640267427b82d918bf
SammriddhGupta <sammriddhgupta@gmail.com>
2024-01-17 23:32:31 +1100

    feat: add converstation chain, make persistent

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # A chatbot for asking questions based on uploaded pdfs
+
+## Built using Langchain and Open AI, hosted using streamlit
--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
 
 # Loop through all the pdfs, loop through all the pages, extract the text and return it as a string
 def get_pdf_text(pdf_docs):
@@ -24,14 +28,30 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text) # split the text into chunks
     return chunks
 
-def get_vectorstore(text_chunks, openai_api_key):
-    embeddings = OpenAIEmbeddings(api_key=openai_api_key)
+def get_vectorstore(text_chunks, api_key):
+    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
     vectorstore = FAISS.from_texts(texts = text_chunks, embedding = embeddings)
     return vectorstore
+
+def get_conversation_chain(vectorstore, api_key):
+    my_llm = ChatOpenAI(openai_api_key=api_key)
+    # now we need to initialise an instance of memory
+    my_memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=my_llm,
+        retriever=vectorstore.as_retriever(),
+        memory=my_memory
+    )
+    return conversation_chain
+    
 
 
 def main():
     st.set_page_config(page_title='Go chat with pdfs', page_icon=':shark:')
+    
+    # checking for and creating session state
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None 
     
     st.header("Chat with pdfs :shark:")
     question = st.text_input("Ask any question about your documents")
@@ -55,6 +75,10 @@ def main():
                 
                 # create vector store
                 vectorstore = get_vectorstore(text_chunks, OPENAI_API_KEY)
+                
+                # create conversation chain 
+                # we do not want streamlit to re-initialise the conversation chain every time we click any button since it rereuns the code, so we use session state
+                st.session_stateconversation = get_conversation_chain(vectorstore, OPENAI_API_KEY)
         
         
             
